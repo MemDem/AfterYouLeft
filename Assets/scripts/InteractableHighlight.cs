8858f630fff8c0f2804a2d6ee5cfa30c7df54b9e
Mehmet Demiray <mehmet.demiray@hotmail.com>
2021-01-29 20:20:55 +0300

    Interactable initial commit

---
+++ Assets/scripts/InteractableHighlight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Interactable nesnelere eklenir. Interactable nesnede asagidaki componentler bulunmalidir.
+//Collider, rigidbody
+public class InteractableHighlight : MonoBehaviour
+{
+    public string interactableName = "";
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //TODO burada metin gosterme coroutine baslayabilir ya da panel aktif hale gelebilir.
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<MeshRenderer>().material.SetColor("_Color", Color.white);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
+    }
+}
